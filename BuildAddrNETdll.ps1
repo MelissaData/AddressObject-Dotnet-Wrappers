3c426ae117b278b5d469cc197ff5102860da00bd
Melissa <ha@melissadata.com>
2023-11-22 08:20:07 -0800

    Tested successfully using Melissa object for 2023.11

---
+++ BuildAddrNETdll.ps1
@@ -0,0 +1,99 @@
+# Name:    BuildAddrNETdll
+# Purpose: Compile the mdAddrNET.dll wrapper 
+
+######################### Parameters ##########################
+
+param($license = '', [switch]$quiet = $false)
+
+######################### Classes ##########################
+
+class FileConfig {
+  [string] $FileName;
+  [string] $ReleaseVersion;
+  [string] $OS;
+  [string] $Compiler;
+  [string] $Architecture;
+  [string] $Type;
+}
+
+######################### Config ###########################
+
+$RELEASE_VERSION = '2023.11'
+
+# Uses the location of the .ps1 file 
+$CurrentPath = $PSScriptRoot
+$CmdPath = "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" # Configure the path to vcvarsall.bat if needed
+
+$Wrapper          = [FileConfig]@{
+  FileName        = "mdAddr_cSharpCode.cs";
+  ReleaseVersion  = $RELEASE_VERSION;
+  OS              = "ANY";
+  Compiler        = "NET";
+  Architecture    = "ANY" ;
+  Type            = "INTERFACE"
+}
+
+######################## Functions #########################
+
+function DownloadWrapper() {
+  Write-Host "MELISSA UPDATER IS DOWNLOADING WRAPPER(S)..."
+
+  # Check for quiet mode
+  if ($quiet) {
+    .\MelissaUpdater\MelissaUpdater.exe file --filename $Wrapper.FileName --release_version $Wrapper.ReleaseVersion --license $LICENSE --os $Wrapper.OS --compiler $Wrapper.Compiler --architecture $Wrapper.Architecture --type $Wrapper.Type --target_directory $CurrentPath > $null
+    if(($?) -eq $False) {
+        Write-Host "`nCannot run Melissa Updater. Please check your license key!"
+        Exit
+    }
+  }
+  else {
+    .\MelissaUpdater\MelissaUpdater.exe file --filename $Wrapper.FileName --release_version $Wrapper.ReleaseVersion --license $LICENSE --os $Wrapper.OS --compiler $Wrapper.Compiler --architecture $Wrapper.Architecture --type $Wrapper.Type --target_directory $CurrentPath 
+    if(($?) -eq $False) {
+        Write-Host "`nCannot run Melissa Updater. Please check your license key!"
+        Exit
+    }
+  }
+
+  Write-Host "Melissa Updater finished downloading " $Wrapper.FileName "!"
+}
+
+########################## Main ############################
+
+Write-Host "`n======================= WELCOME TO MELISSA ADDRESS OBJECT NET WRAPPER =======================`n"
+
+# Get license (either from parameters or user input)
+if ([string]::IsNullOrEmpty($license) ) {
+  $License = Read-Host "Please enter your license key"
+}
+
+# Check for License from Environment Variables 
+if ([string]::IsNullOrEmpty($License) ) {
+  $License = $env:MD_LICENSE 
+}
+
+if ([string]::IsNullOrEmpty($License)) {
+  Write-Host "`nLicense Key is invalid!"
+  Exit
+}
+
+# Download wrapper(s) if Melissa Updater is set up
+if (Test-Path (".\MelissaUpdater\MelissaUpdater.exe")) {
+  DownloadWrapper -license $License
+}
+
+# Start Program
+# Build project
+Write-Host "`n======================================== BUILD WRAPPER ======================================"
+
+cmd.exe /C """$CmdPath"" x86_x64 && csc -out:mdAddrNET.dll -platform:AnyCPU -target:library mdAddr_cSharpCode.cs" > $null
+
+if (Test-Path ("mdAddrNET.dll")) {
+  $filePath = Join-Path -Path (Split-Path -Parent $PSCommandPath) -ChildPath "mdAddrNET.dll"
+
+  Write-Host "`nmdAddrNET.dll has been successfully generated at:`n$filePath`n"
+}
+else {
+  Write-Host "`nError: mdAddrNET.dll was not created successfully`n"
+  Write-Host "`nPlease check for the system requirements and necessary files`n"
+}
+
--- README.md
+++ README.md
@@ -8,12 +8,13 @@ Please feel free to copy or embed this code to your own project. Happy coding!
 
 For latest Melissa Address Object release notes, please visit: https://releasenotes.melissa.com/on-premise-api/address-object/
 
-For further details, please visit: https://www.melissa.com/reference-guides/address-object
+For further details, please visit: https://docs.melissa.com/on-premise-api/address-object/address-object-quickstart.html
 
 ## Tested Environments
 
-- Windows 64-bit .NET 7.0
-- Melissa Address Object for 2023-10
+- Windows 10 64-bit .NET 7.0, Powershell 5.1
+- Linux 64-bit .NET 7.0, Ubuntu 20.04.05 LTS
+- Melissa Address Object for 2023-11
 
 ## Getting Started
 
@@ -25,10 +26,40 @@ $ cd AddressObject-Dotnet-Wrappers
 
 ### Copy wrappers to your project folder
 
-Import the file to your project/solution and include the following line in your code to begin using the wrapper.
+Import `mdAddr_cSharpCode.cs` to your project/solution and include the following line in your code to begin using the wrapper.
 
 ```
 using MelissaData;
+```
+
+## Build mdAddrNET.dll (optional, legacy)
+
+#### Visual Studio Developer Command Prompt
+It is important to note that you must be able to initialize the Visual Studio Developer Command Prompt environment for `x86_x64`. The Visual Studio Developer Command Prompt should already be downloaded if you have Microsoft Visual Studio installed. 
+
+To check if you are able to intialize the Visual Studio Developer Command Prompt for `x86_x64`, you can open the start menu and search for `x86_x64 Cross Tools Command Prompt for VS 2022`. If this program exists, then you may continue to the next steps.
+
+Alternatively, you can check to see if the following filepath exists: `C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat` (with Visual Studio 2022 installed). If the filepath exists, then you may continue to the next steps.
+
+#### Set up Melissa Updater 
+Melissa Updater is a CLI application allowing the user to update their Melissa applications/data. 
+
+- Download Melissa Updater here: <https://releases.melissadata.net/Download/Library/WINDOWS/NET/ANY/latest/MelissaUpdater.exe>
+- Create a folder within the cloned repo called `MelissaUpdater`.
+- Put `MelissaUpdater.exe` in `MelissaUpdater` folder you just created.
+
+#### Run Script
+Parameters:
+- license (optional): a license key to run the powershell script
+
+```
+$ .\BuildAddrNETdll.ps1
+```
+
+or
+
+```
+$ .\BuildAddrNETdll.ps1 -license "<your_license_key>"
 ```
 
 ## See also
