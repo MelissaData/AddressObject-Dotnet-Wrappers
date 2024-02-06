b2eed11612c86c017168dec3d7f07fbb4886a276
Melissa <>
2024-02-06 09:16:49 -0800

    Tested successfully using Melissa object for 2024.01

--- BuildAddrNETdll.ps1
+++ BuildAddrNETdll.ps1
@@ -18,7 +18,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2023.12'
+$RELEASE_VERSION = '2024.01'
 
 # Uses the location of the .ps1 file 
 $CurrentPath = $PSScriptRoot
